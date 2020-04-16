6dea801243afc9a754b2eea5462b70f6dc47e54e
trustmeitsfine <78mariuszsz@gmail.com>
2020-04-16 18:07:22 +0200

    aaaa

--- index.php
+++ index.php
@@ -4,10 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    
 </head>
 <body>
 
-	Mariusz elo joł co tam
-    
+    <div class="Logowanie">
+        <form action="login.php", method="post">
+            <div id="insert_login">
+                Login: <input type="text", name="login">
+            </div>
+            <div id="insert_password">
+            Hasło: <input type="text", name="password">
+            </div>
+            <div id="confirm_button">
+                <input type="submit", value="Zaloguj">
+            </div>
+        </form>
+    </div>
+
+    <div class="Rejestracja">
+        <form action="register.php", method="post">
+            Login: <input type="text", name="login">
+            Hasło: <input type="text", name="password">
+            Powtórz hasło: <input type="text", name="password_repeat">
+            <input type="submit", value="Utwórz konto">
+        </form>
+    </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+    /******************************************************
+    * connection.php
+    * konfiguracja połączenia z bazą danych
+    ******************************************************/
+
+    function connection() {
+        // serwer
+        $mysql_server = "localhost";
+        // admin
+        $mysql_admin = "uzytkownik";
+        // hasło
+        $mysql_pass = "haslo";
+        // nazwa baza
+        $mysql_db = "baza_danych";
+        // nawiązujemy połączenie z serwerem MySQL
+        @mysql_connect($mysql_server, $mysql_admin, $mysql_pass)
+        or die('Brak połączenia z serwerem MySQL.');
+        // łączymy się z bazą danych
+        @mysql_select_db($mysql_db)
+        or die('Błąd wyboru bazy danych.');
+    }
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    echo $login." ".$password;
+
+?>
