cb67aea4a3e5f09505058c887bcbc6383c253c7d
trustmeitsfine <78mariuszsz@gmail.com>
2020-04-18 22:33:15 +0200

    strona gowna

---
+++ strona_gowna.php
@@ -0,0 +1,42 @@
+<!-- Wielkie ambitne planY: -->
+
+<!-- Lista ćwiczeń:  -->
+    <!-- Nazwę -->
+    <!-- Liczba serii -->
+    <!-- Liczba powtózeń w serii -->
+    <!-- Opis ćwiczenia - pojawia się po kilkincięu w ćwiczenei -->
+
+
+<!-- Jakieś coś do wpisanai ile wykonano rzeczywiście serii, ile powtórzeń i komnetarz opcjonalnei -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona Gówna</title>
+
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    
+</head>
+<body>
+
+<!-- Lista ćwiczeń:  -->
+
+    <ol>
+        <!-- Nazwa -->
+        <li>Nazwa Ćwiczenia
+            <ul>
+                <li>Liczba serii</li>
+                <li>Liczba powtózeń w serii</li>
+                <li>Opis ćwiczenia - pojawia się po kilkincięu w ćwiczenei</li>
+            </ul>
+        </li>
+
+    </ol>
+
+    
+</body>
+</html>
